bd472d8f19c5c130d201f50def66040541956616
Elsoft <Elsoft@DESKTOP-PS8S6QV>
2023-02-01 03:08:02 +0100

    Add project files.

---
+++ PokemonReview/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace PokemonReview.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public String   Name   { get; set; }
+
+    }
+}
